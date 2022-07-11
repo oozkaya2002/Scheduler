20201ad80d86778ebf24b5e3a98ad79e697dcbee
oozkaya2002 <98242412+oozkaya2002@users.noreply.github.com>
2022-07-11 18:52:26 -0500

    Fetch Data from URL

--- App.js
+++ App.js
@@ -1,38 +1,26 @@
 import CourseList from './components/CourseList';
-import React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-  
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
+import React, { useState, useEffect } from 'react';
 
-const Banner = ({ title }) => (
-  <Text style={styles.banner}>{ title }</Text>
+const Banner = ({title}) => (
+  <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
 );
 
 const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule =  async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+  
   return (
     <SafeAreaView style={styles.container}>
       <Banner title={schedule.title} />
@@ -40,6 +28,15 @@ const App = () => {
     </SafeAreaView>
   );
 };
+
+// {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Banner title={schedule.title} />
+//       <CourseList courses={schedule.courses} />
+//     </SafeAreaView>
+//   );
+// };
   
 const styles = StyleSheet.create({
   container: {
