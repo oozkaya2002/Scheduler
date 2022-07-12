cd3cd22aeab885be9d612b47926143a947042b91
oozkaya2002 <98242412+oozkaya2002@users.noreply.github.com>
2022-07-11 21:42:15 -0500

    Implemented Course Filtering

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import React from 'react';
 import {View, StyleSheet, ScrollView} from 'react-native';
 import Course from './Course';
+import TermSelector from './TermSelector';
 
-const CourseList = ({ courses }) => (
-  <ScrollView>
-    <View style={styles.courseList}>
-      { courses.map(course => <Course key={course.id} course={ course } />) }
-    </View>
-  </ScrollView>
+const termMap = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const terms = Object.values(termMap);
+
+const getCourseTerm = course => (
+  termMap[course.id.charAt(0)]
 );
+
+const CourseList = ({courses}) => {
+  const [selectedTerm, setSelectedTerm] = useState('Fall');
+
+  const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
+  
+  return (
+    <View>
+      <TermSelector terms={terms} selectedTerm={selectedTerm} setSelectedTerm = {setSelectedTerm} />
+      <ScrollView>
+        <View style={styles.courseList}>
+          { termCourses.map(course => <Course key={course.id} course={course} />) }
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+// const CourseList = ({courses}) => {
+//   const [selectedTerm, setSelectedTerm] = useState('Fall');
+//   const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
+//   return (
+//     <View>
+//       <TermSelector terms={terms}/>
+//       <ScrollView>
+//         <View style={styles.courseList}>
+//           { termCourses.map(course => <Course key={course.id} course={course} />) }
+//         </View>
+//       </ScrollView>
+//     </View>
+//   );
+// };
 
 const styles = StyleSheet.create({
     courseList: {
